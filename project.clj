b2b69f584f6d660289cacc5fb597d57735591355
v <viv@lam>
2012-03-17 12:27:12 -0400

    removing clojure dependency.. keeps pulling in 1.2.1

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject gberenfield/lein-nailgun "2.3.1"
   :description "A leiningen plugin to launch a vimclojure nailgun server."
   :url "http://github.com/mrowl/lein-nailgun"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [vimclojure/server "2.3.1"]])
+  :dependencies [[vimclojure/server "2.3.1"]])
 
